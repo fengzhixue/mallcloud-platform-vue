3267627753de6520715b1b37b0d0b2430ec1c124
nihailong <946816744@qq.com>
2020-04-18 22:47:38 +0800

    更改 api

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://localhost:8081"'
