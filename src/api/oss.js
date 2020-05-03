a5cc407d1b94915d674c4d0af7b25812095415cf
nihailong <946816744@qq.com>
2020-05-03 22:43:11 +0800

    更改 文件上传api

--- src/api/oss.js
+++ src/api/oss.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 export function policy() {
   return request({
-    url:'/aliyun/oss/policy',
+    url:'/api-user/aliyun/oss/policy',
     method:'get',
   })
 }
